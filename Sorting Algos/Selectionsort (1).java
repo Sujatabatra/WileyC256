d18c3da5eb2f82feefd11ae981e3eef1ab83a368
Sujatabatra <sujatabatra@hotmail.com>
2022-07-14 16:08:32 +0530

    Add files via upload

---
+++ Sorting Algos/Selectionsort (1).java
@@ -0,0 +1,42 @@
+package algos;
+//Time Complexity:  O(n2).
+
+public class Selectionsort {
+	static class SelectionSort
+	{
+	    void sort(int arr[])
+	    {
+	        int n = arr.length;
+	   for (int i = 0; i < n-1; i++)
+	        {
+	          
+	            int min_idx = i;
+	            for (int j = i+1; j < n; j++)
+	                if (arr[j] < arr[min_idx])
+	                    min_idx = j;
+	 
+	            int temp = arr[min_idx];
+	            arr[min_idx] = arr[i];
+	            arr[i] = temp;
+	        }
+	    }
+	 
+
+	    void printArray(int arr[])
+	    {
+	        int n = arr.length;
+	        for (int i=0; i<n; ++i)
+	            System.out.print(arr[i]+" ");
+	        System.out.println();
+	    }
+	 
+	    public static void main(String args[])
+	    {
+	        SelectionSort ob = new SelectionSort();
+	        int arr[] = {46,52,21,22,11};
+	        ob.sort(arr);
+	        System.out.println("Sorted array");
+	        ob.printArray(arr);
+	    }
+	}
+}
