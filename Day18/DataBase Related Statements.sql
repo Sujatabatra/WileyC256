f22c588dc0c63a7196c336e7df12f697420b0d7b
Sujatabatra <sujatabatra@hotmail.com>
2022-07-20 12:39:10 +0530

    Add files via upload

---
+++ Day18/DataBase Related Statements.sql
@@ -0,0 +1,19 @@
+show databases;
+
+create database wileyc256;
+
+create database if not exists wileyc256;
+
+drop database wileyc256;
+
+drop database wileyc256;
+drop database IF exists wileyc256;
+
+select database();
+
+select * from sometable;
+
+use wileyc256;
+select database();
+
+select * from sometable;
