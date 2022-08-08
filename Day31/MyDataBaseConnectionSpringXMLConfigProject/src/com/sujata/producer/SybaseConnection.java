65994f691c826ce1f59d8897e0a4ab95cbd09fdc
Sujatabatra <sujatabatra@hotmail.com>
2022-08-08 11:04:00 +0530

    Add files via upload

---
+++ Day31/MyDataBaseConnectionSpringXMLConfigProject/src/com/sujata/producer/SybaseConnection.java
@@ -0,0 +1,33 @@
+package com.sujata.producer;
+
+public class SybaseConnection implements Connection {
+
+	private String url,username,password;
+	
+	@Override
+	public void connect() {
+		System.out.println("Connecting SYBASE with url : "+url+", username : "+username+", password : "+password);
+
+	}
+
+	@Override
+	public void disconnect() {
+		System.out.println("Disconnecting SYBASE!");
+
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
