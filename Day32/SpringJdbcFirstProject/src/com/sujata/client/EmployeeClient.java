0b800664424bf949ff99a831438b59f342e70ac0
Sujatabatra <sujatabatra@hotmail.com>
2022-08-09 14:34:33 +0530

    Add files via upload

---
+++ Day32/SpringJdbcFirstProject/src/com/sujata/client/EmployeeClient.java
@@ -0,0 +1,28 @@
+package com.sujata.client;
+
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sujata.presentation.EmployeePresentation;
+import com.sujata.presentation.EmployeePresentationImpl;
+
+public class EmployeeClient {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("myjdbc.xml");
+		EmployeePresentation employeePresentation=(EmployeePresentationImpl)applicationContext.getBean("employeePresentation");
+		
+		Scanner scanner=new Scanner(System.in);
+		while(true) {
+			employeePresentation.showMenu();
+			System.out.println("Enter Choice ");
+			int choice=scanner.nextInt();
+			employeePresentation.performMenu(choice);
+					
+		}
+
+	}
+
+}
---
+++ Day32/SpringJdbcFirstProject/src/com/sujata/persistence/EmployeeDao.java
@@ -0,0 +1,14 @@
+package com.sujata.persistence;
+
+import java.util.List;
+
+import com.sujata.bean.Employee;
+
+public interface EmployeeDao {
+
+	int addRecord(Employee employee);
+	int deleteRecord(int empId);
+	Employee searchRecord(int empId);
+	List<Employee> getAllRecords();
+	
+}
